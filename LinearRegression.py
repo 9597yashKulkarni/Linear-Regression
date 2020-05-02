82a9b4c474e730df06ab16687d3a511c462e04e3
Yash Kulkarni <yash.kulkarni9597@gmail.com>
2020-05-02 23:27:41 +0530

    Add files via upload

---
+++ LinearRegression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: yashk
+"""
+"""Steps:
+    1.Importing libraries
+    2.Importing dataset
+    3.Splitting dataset
+    4.Fitting simple LR
+    5.Predicting
+    6.Visualizing 
+"""
